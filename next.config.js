bc55f9c023783fad37aa33adef67b9dd52819844
Paul Wild <paulwild@gmail.com>
2022-08-20 14:33:28 +0100

    asset file location

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
+const basePath =
+  process.env.NODE_ENV === "production" ? "/urmston-chess-club" : "";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  assetPrefix: `${basePath}/`,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
