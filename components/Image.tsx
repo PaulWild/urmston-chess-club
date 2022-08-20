587138a93dbedbc5b99c72c95616aea53cf36386
Paul Wild <paulwild@gmail.com>
2022-08-20 15:19:57 +0100

    This might have fixed images

--- components/Image.tsx
+++ components/Image.tsx
@@ -1,7 +1,11 @@
 import NextImage, { ImageLoader, ImageProps } from "next/image";
 
+import getConfig from "next/config";
+
+const { basePath } = getConfig();
+
 const customLoader: ImageLoader = ({ src }) => {
-  return src;
+  return `${basePath}${src}`;
 };
 
 export default function Image(props: ImageProps) {
