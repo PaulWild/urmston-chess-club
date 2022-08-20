a1baa79e3cdaf8e401d3f150523cf5ab4b07234c
PaulWild <paulwild@gmail.com>
2022-08-20 13:25:33 +0000

    Deploying to gh-pages from @ PaulWild/urmston-chess-club@6ad95bd8ed52179cbba7235bedd378a09ed333a4 🚀

---
+++ _next/static/chunks/pages/contact-0f48b3f0fd8007a8.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[335],{1382:function(a,b,c){(window.__NEXT_P=window.__NEXT_P||[]).push(["/contact",function(){return c(5210)}])},5210:function(a,b,c){"use strict";c.r(b);var d=c(5893),e=function(){return(0,d.jsx)("div",{children:"Contact"})};b.default=e}},function(a){a.O(0,[774,888,179],function(){var b;return a(a.s=1382)}),_N_E=a.O()}])
