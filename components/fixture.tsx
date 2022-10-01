333a2e1e617784f9d67b093a8e8c73ef09cfbaf6
Paul Wild <paulwild@gmail.com>
2022-10-01 17:42:41 +0100

    wip;

--- components/fixture.tsx
+++ components/fixture.tsx
@@ -10,10 +10,12 @@ const Fixture = ({ fixture }: Props) => {
   return (
     <section className={styles.fixture}>
       <dl>
+        <dt>League</dt>
+        <dd>{fixture.leaguecup.name}</dd>
         <dt>Home</dt>
-        <dd>{fixture.home}</dd>
+        <dd>{fixture.awayGame ? fixture.opponent : "Urmston"}</dd>
         <dt>Away</dt>
-        <dd>{fixture.away}</dd>
+        <dd>{fixture.awayGame ? "Urmston" : fixture.opponent}</dd>
         <dt>Location</dt>
         <dd>{fixture.location}</dd>
         <dt>Date</dt>
--- pages/fixtures/index.tsx
+++ pages/fixtures/index.tsx
@@ -2,16 +2,19 @@ import { createClient } from "contentful";
 import { Container } from "../../components/container";
 import Fixture from "../../components/fixture";
 
-const client = createClient({
-  space: process.env.CONTENTFUL_SPACE_ID ?? "",
-  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN ?? "",
-});
-
 export type Fixture = {
-  home: string;
-  away: string;
+  sys: {
+    id: string;
+  };
+  opponent: string;
+  awayGame: boolean;
   date: string;
   location: string;
+  homeScore?: number;
+  awayScore?: number;
+  leaguecup: {
+    name: string;
+  };
 };
 
 type Props = {
@@ -19,13 +22,53 @@ type Props = {
 };
 
 export const getStaticProps = async (): Promise<{ props: Props }> => {
-  const res = await client.getEntries<Fixture>({
-    content_type: "fixture",
-    order: "fields.date",
+  const body = JSON.stringify({
+    query: `query {
+  fixtureCollection (order: date_DESC){
+    items{ 
+      sys {
+        id
+      }
+      date, 
+      opponent,
+      location,
+      awayGame,
+      leaguecup {
+        name
+      },
+      homeScore,
+      awayScore,     
+    }
+  }
+}`,
   });
 
+  const res = await fetch(
+    `https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}`,
+    {
+      body,
+      headers: {
+        Authorization: `Bearer ${process.env.CONTENTFUL_ACCESS_TOKEN}`,
+        "content-type": "application/json",
+      },
+      method: "POST",
+    }
+  ).then((x) => x.json());
+
+  console.log(JSON.stringify(res, null, 2));
+  //console.log(JSON.stringify(res, null, 2));
+
+  const items: Fixture[] = res.data.fixtureCollection.items;
+
+  let grouped = new Map<String, Fixture[]>();
+  for (let fixture of items) {
+    let group = grouped.get(fixture.leaguecup.name) ?? [];
+    group.push(fixture);
+    grouped.set(fixture.leaguecup.name, group);
+  }
+
   return {
-    props: { fixtures: res.items.map((x) => x.fields) },
+    props: { fixtures: res.data.fixtureCollection.items },
   };
 };
 
