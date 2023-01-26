11bf956f93c4a8d307efe95311429987fc27bba0
Paul Wild <paul.wild@coop.co.uk>
2023-01-26 13:20:49 +0000

    sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+
+module.exports = {
+  siteUrl: "https://www.urmstonchessclub.com",
+  generateRobotsTxt: true,
+  outDir: "out",
+  changefreq: "monthly",
+};
