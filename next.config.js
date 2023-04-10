b57d6bccc41365e645d3aef856c1197a9517c231
Paul Wild <paulwild@gmail.com>
2023-04-10 15:03:21 +0100

    vercel pipelines

--- next.config.js
+++ next.config.js
@@ -3,9 +3,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  images: {
-    unoptimized: true,
-  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
