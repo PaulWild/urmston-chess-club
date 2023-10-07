96a05a7cd7f43935b2358ea3147be60fd922e31a
Paul Wild <paulwild@gmail.com>
2023-10-07 16:12:05 +0100

    every page has a banner

--- app/links/page.tsx
+++ app/links/page.tsx
@@ -1,22 +1,27 @@
 import { HiExternalLink } from "react-icons/hi";
 import { Container } from "../../components/container";
+import chessImage from "./../chess-board.jpg";
+import Banner from "../../components/banner";
 
 export default function Page() {
   return (
-    <Container>
-      <h1>Links</h1>
-      <ul>
-        <li>
-          <a href="https://www.manchesterchess.com/">
-            Manchester Chess Federation <HiExternalLink />
-          </a>
-        </li>
-        <li>
-          <a href="https://www.englishchess.org.uk/">
-            English Chess Federation <HiExternalLink />
-          </a>
-        </li>
-      </ul>
-    </Container>
+    <>
+      <Banner imgPath={chessImage} />
+      <Container>
+        <h1>Links</h1>
+        <ul>
+          <li>
+            <a href="https://www.manchesterchess.com/">
+              Manchester Chess Federation <HiExternalLink />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.englishchess.org.uk/">
+              English Chess Federation <HiExternalLink />
+            </a>
+          </li>
+        </ul>
+      </Container>
+    </>
   );
 }
