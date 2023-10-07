0dc87d3e0624ffccdc581346819f7dacea07ad03
Paul Wild <paulwild@gmail.com>
2023-10-07 19:39:51 +0100

    hmm

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,11 +18,7 @@ export default function RootLayout({
     <html lang="en-GB">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="true"
-        />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Permanent+Marker&family=Roboto:wght@100;300;400;500;700&display=swap"
           rel="stylesheet"
